d286ddfcbf388dae8bc9f5db872fbc92af1c50e0
TopScrech <sergei_saliukov@icloud.com>
2024-06-02 09:02:42 +0200

    Initial Commit

---
+++ File Hider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  File Hider
+//
+//  Created by Sergei Saliukov on 02/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ File Hider/File_HiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_HiderApp.swift
+//  File Hider
+//
+//  Created by Sergei Saliukov on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct File_HiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
