e5ef9a3eda2b96a0c80baf650b1da7bbecc1cc8d
TopScrech <sergei_saliukov@icloud.com>
2025-05-08 14:02:34 +0200

    Printing the path of dropped files

--- File Hider/Content View.swift
+++ File Hider/Content View.swift
@@ -1,14 +1,43 @@
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct ContentView: View {
+    @State private var isTargeted = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Goida")
         }
-        .padding()
+        .background {
+            if isTargeted {
+                Color.blue.opacity(0.2)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onDrop(of: [.fileURL], isTargeted: $isTargeted) { providers in
+            for provider in providers {
+                provider.loadItem(forTypeIdentifier: UTType.fileURL.identifier, options: nil) { item, error in
+                    if let error {
+                        print("Error:", error)
+                        return
+                    }
+                    
+                    guard
+                        let data = item as? Data,
+                        let url = NSURL(
+                            absoluteURLWithDataRepresentation: data,
+                            relativeTo: nil
+                        ) as URL?
+                    else {
+                        return
+                    }
+                    
+                    print(url.path)
+                }
+            }
+            
+            return true
+        }
     }
 }
 
