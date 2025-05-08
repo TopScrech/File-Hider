fd60659cc05f645a599ab7f40b4c2f19d8092697
TopScrech <sergei_saliukov@icloud.com>
2025-05-08 15:06:16 +0200

    Hide/unhide files on drag & drop

--- File Hider/Content View.swift
+++ File Hider/Content View.swift
@@ -2,41 +2,80 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 struct ContentView: View {
-    @State private var isTargeted = false
-    
+    @State private var isHideTargeted = false
+    @State private var isUnhideTargeted = false
+    //defaults write com.apple.finder AppleShowAllFiles TRUE
     var body: some View {
-        VStack {
-            Text("Goida")
-        }
-        .background {
-            if isTargeted {
-                Color.blue.opacity(0.2)
+        HStack {
+            VStack {
+                Text("Hide")
             }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .onDrop(of: [.fileURL], isTargeted: $isTargeted) { providers in
-            for provider in providers {
-                provider.loadItem(forTypeIdentifier: UTType.fileURL.identifier, options: nil) { item, error in
-                    if let error {
-                        print("Error:", error)
-                        return
-                    }
-                    
-                    guard
-                        let data = item as? Data,
-                        let url = NSURL(
-                            absoluteURLWithDataRepresentation: data,
-                            relativeTo: nil
-                        ) as URL?
-                    else {
-                        return
-                    }
-                    
-                    print(url.path)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .onDrop(of: [.fileURL], isTargeted: $isHideTargeted) { providers in
+                processFile(providers)
+            }
+            .background {
+                if isHideTargeted {
+                    Color.blue.opacity(0.5)
                 }
             }
             
-            return true
+            VStack {
+                Text("Unhide")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .onDrop(of: [.fileURL], isTargeted: $isUnhideTargeted) { providers in
+                processFile(providers, hide: false)
+            }
+            .background {
+                if isUnhideTargeted {
+                    Color.blue.opacity(0.5)
+                }
+            }
+        }
+    }
+    
+    private func processFile(
+        _ providers: [NSItemProvider],
+        hide: Bool = true
+    ) -> Bool {
+        for provider in providers {
+            provider.loadItem(
+                forTypeIdentifier: UTType.fileURL.identifier,
+                options: nil
+            ) { item, error in
+                if let error {
+                    print("Error:", error)
+                    return
+                }
+                
+                guard
+                    let data = item as? Data,
+                    let url = NSURL(
+                        absoluteURLWithDataRepresentation: data,
+                        relativeTo: nil
+                    ) as URL?
+                else {
+                    return
+                }
+                
+                print(url.path)
+                makeFileHidden(url, hide: hide)
+            }
+        }
+        
+        return true
+    }
+    
+    private func makeFileHidden(_ url: URL, hide: Bool = true) {
+        var resourceValues = URLResourceValues()
+        resourceValues.isHidden = hide
+        
+        do {
+            var modifiableURL = url
+            try modifiableURL.setResourceValues(resourceValues)
+        } catch {
+            print("Error:", error.localizedDescription)
         }
     }
 }
