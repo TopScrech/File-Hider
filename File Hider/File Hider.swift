00de8cbe648fcff5c29157181f10b72900dc0cfe
TopScrech <sergei_saliukov@icloud.com>
2025-05-08 13:55:23 +0200

    -

--- File Hider/File Hider.swift
+++ File Hider/File Hider.swift
@@ -1,14 +1,7 @@
-//
-//  File_HiderApp.swift
-//  File Hider
-//
-//  Created by Sergei Saliukov on 02/06/2024.
-//
-
 import SwiftUI
 
 @main
-struct File_HiderApp: App {
+struct FileHider: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
